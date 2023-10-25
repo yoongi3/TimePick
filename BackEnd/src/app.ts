778b333ac924c05e8d7a1469a722c601a775f5f5
Josh <lolhongy3@gmail.com>
2023-10-26 00:07:23 +1100

    initial backend commit

---
+++ BackEnd/src/app.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+export const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
